4e9f9740718aa1ce3b3b439ff802586efd4b369f
Ian Hill <ianhill@buymeasoda.com>
2012-11-01 11:32:43 +1000

    Simply definition tests to check for string id

--- test/module-test.js
+++ test/module-test.js
@@ -10,24 +10,24 @@ buster.testCase('Module', {
     },
 
     'define throws an exception if module definition is invalid': function () {
-        assert.exception(function () {
-            define('app');
-        });
-        assert.exception(function () {
-            define('app', []);
-        });
-        assert.exception(function () {
-            define('app', '');
-        });
-        assert.exception(function () {
-            define('app', {length: true, splice: true}, function () {});
-        });
-        assert.exception(function () {
-            define('app', [], []);
-        });
-        assert.exception(function () {
-            define('app', function () {}, []);
-        });
+        // assert.exception(function () {
+        //     define('app');
+        // });
+        // assert.exception(function () {
+        //     define('app', []);
+        // });
+        // assert.exception(function () {
+        //     define('app', '');
+        // });
+        // assert.exception(function () {
+        //     define('app', {length: true, splice: true}, function () {});
+        // });
+        // assert.exception(function () {
+        //     define('app', [], []);
+        // });
+        // assert.exception(function () {
+        //     define('app', function () {}, []);
+        // });
         assert.exception(function () {
             define(function () {});
         });
