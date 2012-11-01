d5fc4eb00738a7a8001ee2204b7f23955efe4fb4
Ian Hill <ianhill@buymeasoda.com>
2012-11-01 11:22:22 +1000

    Add tests for incorrect define parameters

--- test/module-test.js
+++ test/module-test.js
@@ -3,6 +3,36 @@ buster.testCase('Module', {
     'define and require functions exist': function () {
         assert.isFunction(define);
         assert.isFunction(require);
+    },
+
+    'define throws an exception if module definition is invalid': function () {
+        assert.exception(function () {
+            define('app');
+        });
+        assert.exception(function () {
+            define('app', []);
+        });
+        assert.exception(function () {
+            define('app', '');
+        });
+        assert.exception(function () {
+            define('app', {length: true, splice: true}, function () {});
+        });
+        assert.exception(function () {
+            define('app', [], []);
+        });
+        assert.exception(function () {
+            define('app', function () {}, []);
+        });
+        assert.exception(function () {
+            define(function () {});
+        });
+        assert.exception(function () {
+            define([], function () {});
+        });
+        assert.exception(function () {
+            define({}, [], function () {});
+        });
     }
 
 });
